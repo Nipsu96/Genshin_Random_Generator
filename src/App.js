b476fe43d91ed57602ef5a103021539641a62c3e
PastelSnowChild <n.lahteinen1@gmail.com>
2022-09-24 23:06:29 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { characterList } from './characters';
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/CharacterGenerator" element={<CharacterList />} />
+      </Routes>
     </div>
   );
 }
+function Home() {
+  return (
+    <>
+      <main className="FrontPage">
+        <h2>Welcome to the Random Character Generator!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/CharacterGenerator"><button className='homebutton'>Go wild</button></Link>
+      </nav>
+    </>
+  );
+}
 
+function CharacterList() {
+  const [num, setNum] = useState(null);
+  const [characterName, setCharacterName] = useState("");
+  const [show, setShow] = useState(false);
+
+
+  function randomNumberInRange(min, max) {
+    // 👇️ get number between min (inclusive) and max (inclusive)
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  const handleClick = () => {
+    setNum(randomNumberInRange(0, characterList.length - 1));
+    fetchCharacter();
+    setShow(true)
+  };
+  const fetchCharacter = () => {
+    if (num == null) {
+
+    } else {
+      setCharacterName(characterList[num].characterName);
+    }
+
+  };
+
+  return (
+    <div className="content" >
+      <h2>Character is: {num === null ? characterName : characterList[num].characterName}</h2>
+      <button onClick={handleClick}>Generate random character</button>
+      {
+        show ? <img id="CharacterPic" src={characterList[num].url} alt={characterList[num].characterName} /> : <p></p>
+      }
+    </div >
+  );
+};
 export default App;
---
+++ src/characters.js
@@ -0,0 +1,125 @@
+import Albedo from './img/Albedo.png'
+import Aloy from './img/Aloy.png'
+import Amber from './img/Amber.png'
+import Arataki_Itto from './img/Arataki_Itto.png'
+import Barbara from './img/Barbara.png'
+import Beidou from './img/Beidou.png'
+import Bennet from './img/Bennett.png'
+import Chongyun from './img/Chongyun.png'
+import Collei from './img/Collei.png'
+import Diluc from './img/Diluc.png'
+import Diona from './img/Diona.png'
+import Dori from './img/Dori.png'
+import Eula from './img/Eula.png'
+import Fischl from './img/Fischl.png'
+import Ganyu from './img/Ganyu.png'
+import Gorou from './img/Gorou.png'
+import Hu_Tao from './img/Hu_Tao.png'
+import Jean from './img/Jean.png'
+import Kaedehara_Kazuha from './img/Kaedehara_Kazuha.png'
+import Kaeya from './img/Kaeya.png'
+import Kamisato_Ayaka from './img/Kamisato_Ayaka.png'
+import Kamisato_Ayato from './img/Kamisato_Ayato.png'
+import Keqing from './img/Keqing.png'
+import Klee from './img/Klee.png'
+import Kujou_Sara from './img/Kujou_Sara.png'
+import Kuki_Shinobu from './img/Kuki_Shinobu.png'
+import Lisa from './img/Lisa.png'
+import Mona from './img/Mona.png'
+import Ningguang from './img/Ningguang.png'
+import Noelle from './img/Noelle.png'
+import Qiqi from './img/Qiqi.png'
+import Raiden_Shogun from './img/Raiden_Shogun.png'
+import Razor from './img/Razor.png'
+import Rosaria from './img/Rosaria.png'
+import Sangonomiya_Kokomi from './img/Sangonomiya_Kokomi.png'
+import Sayu from './img/Sayu.png'
+import Shenhe from './img/Shenhe.png'
+import Shikanoin_Heizou from './img/Shikanoin_Heizou.png'
+import Sucrose from './img/Sucrose.png'
+import Tartaglia from './img/Tartaglia.png'
+import Thoma from './img/Thoma.png'
+import Tighnari from './img/Tighnari.png'
+import traveler_Anemo from './img/traveler(Anemo).png'
+import traveler_Dendro from './img/traveler(Dendro).png'
+import traveler_Electro from './img/traveler(Electro).png'
+import traveler_Geo from './img/traveler(Geo).png'
+import Venti from './img/Venti.png'
+import Xiangling from './img/Xiangling.png'
+import Xiao from './img/Xiao.png'
+import Xingqiu from './img/Xingqiu.png'
+import Xinyan from './img/Xinyan.png'
+import Yae_Miko from './img/Yae_Miko.png'
+import Yanfei from './img/Yanfei.png'
+import Yelan from './img/Yelan.png'
+import Yoimiya from './img/Yoimiya.png'
+import Yun_Jin from './img/Yun_Jin.png'
+import Zhongli from './img/Zhongli.png'
+
+
+
+
+export const characterList = [
+    { characterName: "Albedo", url: Albedo },
+    { characterName: "Aloy", url: Aloy },
+    { characterName: "Amber", url: Amber },
+    { characterName: "Arataki Itto", url: Arataki_Itto },
+    { characterName: "Barbara", url: Barbara },
+    { characterName: "Beidou ", url: Beidou },
+    { characterName: "Bennet ", url: Bennet },
+    { characterName: "Chongyun ", url: Chongyun },
+    { characterName: "Collei", url: Collei },
+    { characterName: "Diluc", url: Diluc },
+    { characterName: "Diona", url: Diona },
+    { characterName: "Dori", url: Dori },
+    { characterName: "Eula", url: Eula },
+    { characterName: "Fischl", url: Fischl },
+    { characterName: "Ganyu", url: Ganyu },
+    { characterName: "Gorou", url: Gorou },
+    { characterName: "Hu Tao", url: Hu_Tao },
+    { characterName: "Jean", url: Jean },
+    { characterName: "Kaedehara Kazuha", url: Kaedehara_Kazuha },
+    { characterName: "Kaeya", url: Kaeya },
+    { characterName: "Kamisato Ayaka", url: Kamisato_Ayaka },
+    { characterName: "Kamisato Ayato", url: Kamisato_Ayato },
+    { characterName: "Keqing", url: Keqing },
+    { characterName: "Klee", url: Klee },
+    { characterName: "Kujou Sara", url: Kujou_Sara },
+    { characterName: "Kuki Shinobu", url: Kuki_Shinobu },
+    { characterName: "Lisa", url: Lisa },
+    { characterName: "Mona", url: Mona },
+    { characterName: "Ningguang", url: Ningguang },
+    { characterName: "Noelle", url: Noelle },
+    { characterName: "Qiqi", url: Qiqi },
+    { characterName: "Raiden Shogun", url: Raiden_Shogun },
+    { characterName: "Razor", url: Razor },
+    { characterName: "Rosaria", url: Rosaria },
+    { characterName: "Sangonomiya Kokomi", url: Sangonomiya_Kokomi },
+    { characterName: "Sayu", url: Sayu },
+    { characterName: "Shenhe", url: Shenhe },
+    { characterName: "Shikanoin Heizou", url: Shikanoin_Heizou },
+    { characterName: "Sucrose", url: Sucrose },
+    { characterName: "Tartaglia", url: Tartaglia },
+    { characterName: "Thoma", url: Thoma },
+    { characterName: "Tighnari", url: Tighnari },
+    { characterName: "Traveler(Anemo)", url: traveler_Anemo },
+    { characterName: "Traveler(Dendro)", url: traveler_Dendro },
+    { characterName: "Traveler(Electro)", url: traveler_Electro },
+    { characterName: "Traveler(Geo)", url: traveler_Geo },
+    { characterName: "Venti", url: Venti },
+    { characterName: "Xiangling", url: Xiangling },
+    { characterName: "Xiao", url: Xiao },
+    { characterName: "Xingqiu", url: Xingqiu },
+    { characterName: "Xinyan", url: Xinyan },
+    { characterName: "Yae Miko", url: Yae_Miko },
+    { characterName: "Yanfei", url: Yanfei },
+    { characterName: "Yelan", url: Yelan },
+    { characterName: "Yoimiya", url: Yoimiya },
+    { characterName: "Yun Jin", url: Yun_Jin },
+    { characterName: "Zhongli", url: Zhongli },
+]
+
+
+export default {
+    characterList,
+}
